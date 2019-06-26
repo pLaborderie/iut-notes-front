4bb222c7ce015f142429dc7609aaa92859e8ff17
Paul Laborderie <paul.laborderie@gmail.com>
2019-06-26 19:54:02 +0200

    [FIX] - Setting headers on each request

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,13 +1,19 @@
 import ApolloClient from 'apollo-boost';
 
-const token = localStorage.getItem('iut-notes-jwt');
 const { protocol, hostname, port } = window.location;
+
 const uri = process.env.NODE_ENV !== 'production'
   ? 'http://localhost:4000/graphql'
   : `${protocol}//${hostname}:${port}/graphql`
+
 export default new ApolloClient({
   uri,
-  headers: {
-    authorization: token ? `Bearer ${token}` : ''
-  },
+  request: operation => {
+    const token = localStorage.getItem('iut-notes-jwt');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  }
 });
